712056a61a6acff8f221a0625f4b6b9e0b02fd2d
gee44njuguna <gee44njuguna@gmail.com>
2026-01-18 15:50:46 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+import telebot
+import os
+
+TOKEN = os.environ.get("8211711661:AAHctNqA37TY96PxSVMtkXSjqtz0lhRs6Rg")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "👋 Welcome to Cyber Awareness!\nI am Digital Detective 254.\nI teach cybersecurity, hacking basics, OSINT and programming.")
+
+@bot.message_handler(func=lambda m: True)
+def reply_all(message):
+    bot.reply_to(message, "🕵️ Digital Detective 254:\nAsk me anything about cybersecurity, hacking, investigations or programming.")
+
+print("Bot running...")
+bot.infinity_polling()
